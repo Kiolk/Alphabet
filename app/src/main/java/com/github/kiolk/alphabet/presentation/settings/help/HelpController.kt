9cdcf50fbec142ff2581d2f049b1c512e36b09c9
Yauheni Slizh <eugene.slizh>
2019-08-16 07:53:52 +0300

    Refactored layout of help screen in settings

--- app/src/main/java/com/github/kiolk/alphabet/presentation/settings/help/HelpController.kt
+++ app/src/main/java/com/github/kiolk/alphabet/presentation/settings/help/HelpController.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import butterknife.BindView
+import butterknife.OnClick
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.ProvidePresenter
 import com.github.kiolk.alphabet.R
 import com.github.kiolk.alphabet.presentation.base.controller.BaseController
+import kotlinx.android.synthetic.main.controller_help.view.*
 
 class HelpController: BaseController, HelpView {
 
@@ -18,6 +21,26 @@ class HelpController: BaseController, HelpView {
     constructor(args: Bundle): super(args)
 
     override fun inflateView(inflater: LayoutInflater, container: ViewGroup): View = inflater.inflate(R.layout.controller_help, container, false)
+
+    @OnClick(R.id.ll_help_communicate)
+    fun onCommunicatePress(){
+
+    }
+
+    @OnClick(R.id.ll_help_add)
+    fun onAddWordsPress(){
+
+    }
+
+    @OnClick(R.id.ll_help_finance)
+    fun onFinancePress(){
+
+    }
+
+    @OnClick(R.id.ll_help_improve)
+    fun onImprovePress(){
+
+    }
 
     @ProvidePresenter
     fun providePresenter(): HelpPresenter{
